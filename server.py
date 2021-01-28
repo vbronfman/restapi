c4afe71f55db46b5b425493a83e56c3c8723c810
talilabok <74266474+talilabok@users.noreply.github.com>
2021-01-28 11:05:30 +0200

    fixed main

--- server.py
+++ server.py
@@ -54,4 +54,4 @@ api.add_resource(Employees_Title_Update,'/employees_update/<employee_id>') #atua
 
 
 if __name__ == '__main__':
-     app.run(host='localhost', port=5000)
+     app.run(host='0.0.0.0', port=5000)
